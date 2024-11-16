6e239223d55365b8338223bfafa681dd8b3d1509
rk17-max <143068154+rk17-max@users.noreply.github.com>
2024-11-17 01:22:06 +0530

    Update BinaryTree.h

--- DataStructureVisualizer/BinaryTree.h
+++ DataStructureVisualizer/BinaryTree.h
@@ -1,1 +1,47 @@
 
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+#include <vector>
+#include <iostream>
+
+class BinaryTreeNode {
+public:
+    int data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+
+    BinaryTreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+class BinaryTree {
+private:
+    BinaryTreeNode* root;
+
+    // Helper methods
+    void insertRecursive(BinaryTreeNode* &node, int value);
+    void inorderTraversalRecursive(BinaryTreeNode* node, std::vector<int>& result);
+    void preorderTraversalRecursive(BinaryTreeNode* node, std::vector<int>& result);
+    void postorderTraversalRecursive(BinaryTreeNode* node, std::vector<int>& result);
+
+public:
+    BinaryTree() : root(nullptr) {}
+
+    // Insertion methods
+    void insert(int value);
+
+    // Traversal methods
+    std::vector<int> inorderTraversal();
+    std::vector<int> preorderTraversal();
+    std::vector<int> postorderTraversal();
+
+    // Search and deletion methods
+    bool search(int value);
+    void remove(int value);
+
+    // Utility methods
+    int getHeight();
+    void clear();
+};
+
+#endif // BINARY_TREE_H
